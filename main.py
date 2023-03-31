c9530e415b54f2cb8ed009f15ca045e8ad513ece
Dildora <48212@edu.raseko.fi>
2023-03-31 10:13:23 +0300

    lisä
    lisä

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 # MAIN WINDOW FOR FITNESS APPLICATION
 # ===================================
 
-# LIBRARY AND MODULES
+# LIBRARIES AND MODULES
 import sys
 from PyQt6 import QtCore # Core functionality of Qt
 from PyQt6 import QtWidgets # UI elements functionality
 from PyQt6.uic.load_ui import loadUi
 
-# Class for the main Window
+# Class for the main window
 class MainWindow(QtWidgets.QMainWindow):
 
     """MainWindow for the fitness app"""
-    
-    # Constructor for the main Window
+
+    # Constructor for the main window
     def __init__(self):
-        super().__init__()   
+        super().__init__()
 
         # Load the UI file
-        loadUi('main.ui', self)
+        self.main = loadUi('main.ui', self)
 
         # Define UI Controls ie buttons and input fields
         self.calculatePB = self.calculatePushButton
-        self.calculatePB.clicked.connect(self.calculateAll)           
+        self.calculatePB.clicked.connect(self.calculateAll)
 
     # Define slots ie methods
     def calculateAll(self):
-        self.bmiLabel.setText('100')
+        self.bmiLabel.setValue('100')
 
 if __name__ == "__main__":
-    
     # Create the application
     app = QtWidgets.QApplication(sys.argv)
 
     # Create the Main Window object from MainWindow class and show it on the screen
     appWindow = MainWindow()
-    appWindow.show()
+    appWindow.main.show()
     sys.exit(app.exec())
-
 
     # Create the mainwindow (and show it)
  
